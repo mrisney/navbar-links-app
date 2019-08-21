067dbb3168dd5be36c6ec246e78b41b31b49e80a
mrisney <marc.risney@gmail.com>
2019-08-21 01:47:10 -0500

    update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to angular-httpclient-app!');
+    expect(page.getParagraphText()).toEqual('Welcome to navbar-links-app!');
   });
 });
